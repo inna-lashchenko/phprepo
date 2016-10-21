1fe36a245fbeef16a9909f47de50a2b3a6a24cf5
Inna <inn4633@gmail.com>
2016-10-21 17:53:47 +0300

    changes

--- main/mail/change.php
+++ main/mail/change.php
@@ -8,27 +8,45 @@ $phone = $_POST["phone"];
 $message = $_POST["message"];
 $id = $_POST["id"];
 
+if (empty($phone)){
+    echo "<span style='margin-left: 20px; color:lightseagreen; font-weight: bold;'>Enter phone! ;)</span><br><br>";
 
+}
+else{
+    try {
+        $conn = new PDO('mysql:host=localhost; dbname=test; charset=utf8', $username, $password );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->beginTransaction();
 
-try {
-    $conn = new PDO('mysql:host=localhost; dbname=test; charset=utf8', $username, $password );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->beginTransaction();
-
-    $sql = ("UPDATE test SET name='$name', email='$email', phone='$phone', message='$message' WHERE id=$id");
-
-    $count = $conn->exec($sql);
-    if ($count == 0){
-        $sql = ("INSERT INTO test (id, name, email, phone, message) values ('$id','$name', '$email', '$phone', '$message')");
+        $sql = ("UPDATE test SET `name`='$name', email='$email', phone='$phone', message='$message' WHERE id=$id");
         $count = $conn->exec($sql);
+
+        if ($count == 0){
+            $stmt = ("SELECT * FROM test WHERE id=$id");
+            $result = $conn->query($stmt);
+            $result = $result->fetchColumn();
+            if($result==false){
+                $sql = ("INSERT INTO test (id, name, email, phone, message) values ('$id','$name', '$email', '$phone', '$message')");
+                $count = $conn->exec($sql);
+                echo "<span style='margin-left: 20px; color:lightseagreen; font-weight: bold;'>Data updated! ;)</span><br><br>";
+
+            }
+
+            else{
+                echo "<span style='margin-left: 20px; color:lightseagreen; font-weight: bold;'>Nothing to update! ;)</span><br><br>";
+            }
+
+        }
+        else echo "<span style='margin-left: 20px; color:lightseagreen; font-weight: bold;'>Data updated! ;)</span><br><br>";
+
+        $conn->commit();
+
+
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
     }
 
-    $conn->commit();
-    echo "<span style='margin-left: 20px; color:lightseagreen; font-weight: bold;'>Data updated! ;)</span><br><br>";
-
-
 }
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-}
+
--- main/mail/validation.php
+++ main/mail/validation.php
@@ -1,7 +1,81 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inna
- * Date: 19.10.16
- * Time: 16:18
- */
+function clean($value) {
+$value = trim($value);
+$value = stripslashes($value);
+$value = strip_tags($value);
+$value = htmlspecialchars($value);
+
+return $value;
+}
+function check_length($value, $min, $max) {
+$result = (mb_strlen($value) < $min || mb_strlen($value) > $max);
+return $result;
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $kek = 0;
+
+
+    $name = clean($name);
+    $phone = clean($phone);
+    $email = clean($email);
+    $message = clean($message);
+    if (empty($name) && empty($phone) && empty($email) && empty($message)) {
+        echo "Fill empty fields";
+    }
+    else {
+        $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if(!preg_match("/^[а-яА-ЯёЁa-zA-Z]/",$name)){
+            echo "Incorrect name";
+        }
+            elseif (!preg_match("/^[0-9]/", $phone)||check_length($phone,6,15)){
+                echo "Incorrect phone";
+            }
+            elseif (!$email_validate){
+                echo "Incorrect email";
+            }
+            elseif (check_length($message,2,1000)){
+                echo "Incorrect message";
+            }
+            else
+            {
+                echo "OK";
+                $kek = 1;
+            }
+
+    }
+}
+if($kek==1){
+    $username = "root";
+    $password = "kostyaklushkin23";
+
+    try {
+        $conn = new PDO('mysql:host=localhost; dbname=test; charset=utf8', $username, $password );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->beginTransaction();
+
+        $sql = ("INSERT INTO test (name, email, phone, message) values ('$name', '$email', '$phone', '$message')");
+        $conn->exec($sql);
+        $conn->commit();
+
+        $stmt = $conn->prepare("SELECT id, name, email, phone, message FROM test");
+        $stmt->execute();
+
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $values = $stmt->fetchAll();
+
+
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+
+
